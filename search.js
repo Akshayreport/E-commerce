632975d444d954d30be6b5b62ce47302fc0468af
Akshayreport <49643455+Akshayreport@users.noreply.github.com>
2023-01-27 16:34:58 +0000

    Delete search.js

--- search.js
+++
@@ -1,3 +0,0 @@
-const searchKey = decodeURI(location.pathname.split('/').pop());
-console.log(searchKey);
-getProducts(searchKey).then(data => createProductCards(data, searchKey, '.search-listing'))
